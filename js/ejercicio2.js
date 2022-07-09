5ce11bdfb21117a005bd502ab588ceee5e671ca3
Pereyra Jose <luispereyra14@hotmail.com>
2022-07-09 17:22:28 -0300

    ejercicio2Terminado

---
+++ js/ejercicio2.js
@@ -0,0 +1,41 @@
+let nota = parseInt(prompt("Escriba la nota del 0 al 10"))
+
+switch(nota){
+
+    case 0:
+        document.write("Muy deficiente");
+        break
+    case 1:
+        document.write("Muy deficiente");
+        break
+    case 2:
+        document.write("Muy deficiente");
+        break
+    case 3:
+        document.write("Insuficiente");
+        break
+    case 4:
+        document.write("Insuficiente");
+        break
+    case 5:
+        document.write("suficiente");
+        break
+    case 6:
+        document.write("suficiente");
+        break
+    case 7:
+        document.write("Bien");
+        break
+    case 8:
+        document.write("Muy Bien");
+        break
+    case 9:
+        document.write("Muy Bien");
+        break
+    case 10:
+        document.write("Sobresaliente");
+        break
+    default:
+        document.write("La nota elegida no es valida")
+}
+
