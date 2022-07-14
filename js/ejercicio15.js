f610a0ea5bcf722b44a37b61dbac1846a32e5980
Pereyra Jose <luispereyra14@hotmail.com>
2022-07-13 21:12:03 -0300

    ejercicio14Terminado

---
+++ js/ejercicio15.js
@@ -0,0 +1,13 @@
+let cadena = prompt("Introduce una cadena de texto:");
+let numchar = cadena.length;    //Devuelve la longitud del string.
+cadena = cadena.toUpperCase();  //Devuelve el string en mayúsculas.
+let car;
+let contador = 0;
+let i;
+for (i = 0; i < numchar; i++) {
+    car = cadena.charAt(i);    //recupera el caracter i del string.
+    if ((car == "A") || (car == "E") || (car == "I") || (car == "O") || (car == "U")) {
+        contador++;
+    }
+}
+document.write("Número de Vocales: " + contador + ".");
