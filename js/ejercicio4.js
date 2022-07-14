8ef3de1cb48475fe3464909c8c9179b0089781f6
Pereyra Jose <luispereyra14@hotmail.com>
2022-07-13 21:38:59 -0300

    tp2JSTerminado

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,18 +1,10 @@
-var suma = 0;
-
+let suma = 0;
 do {
-  var numero = prompt("Introduce un numero");
-
-  if (Number(numero) == numero) {
-    numero = Number(numero);
-
+  let numero = parseInt(prompt("Introduce un numero"));
+  if (numero = Number(numero)) {
     suma = suma + numero;
-  } else {
-    if (numero != undefined) {
-      alert(numero + " No es un numero");
-    }
+  } else if (numero != Number(numero)) {
+    alert("El valor ingresado NO es un numero");
   }
-} while (numero != undefined);
-
+} while (confirm("¿Desea seguir escribiendo numeros?"));
 document.write(suma);
-
